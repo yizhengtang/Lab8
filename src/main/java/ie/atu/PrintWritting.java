79752172731324e696faedf14ad883a9a45885ab
Yi Zheng Tang <yzhengtang4@gmail.com>
2023-11-20 17:27:19 +0000

    Created PrintWriter as a new way to add text to text file.

---
+++ src/main/java/ie/atu/PrintWritting.java
@@ -0,0 +1,26 @@
+package ie.atu;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import java.io.File;
+
+public class PrintWritting {
+    public static void main(String[] args) {
+        File myFile = new File ("PrintWrite.txt");
+
+        try (PrintWriter text = new PrintWriter (new FileWriter(myFile, true)))
+        {
+            text.println("Hello.");
+            text.println("My name is Yi Zheng Tang, Michael.");
+
+            System.out.println("Text added into text file successfully!");
+        }
+
+        catch (IOException e)
+        {
+            System.out.println("Error occurred while adding text to the text file.");
+        }
+    }
+}
